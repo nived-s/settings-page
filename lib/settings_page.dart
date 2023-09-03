5c216ee380349622f9c40bd19fd7e6adaf0834bb
nived-s <nivedsreenilayam@gmail.com>
2023-09-03 22:23:02 +0530

    first commit base file

---
+++ lib/settings_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [Text('data')],
+        ),
+      ),
+    );
+  }
+}
